487458e757cd142af3631e438b8a0d011c2d0c58
Aleksander Spro <abspro@gmail.com>
2022-12-15 15:53:50 +0100

    Fix seeling from top inventory

--- modules/looter/sell.lua
+++ modules/looter/sell.lua
@@ -51,7 +51,7 @@ local function sellItem(itemToSell)
     if(itemToSell.ItemSlot2() >= 0) then
       mq.cmdf("/nomodkey /itemnotify in pack%d %d leftmouseup", packslot, itemToSell.ItemSlot2() + 1)
     else
-      mq.cmdf("/nomodkey /itemnotify in pack%d leftmouseup", packslot)
+      mq.cmdf("/nomodkey /itemnotify pack%d leftmouseup", packslot)
     end
 
     mq.delay(retryTimer:TimeRemaining(), function() return merchantWindow.Child("MW_SelectedItemLabel").Text() == itemToSell.Name() end)
