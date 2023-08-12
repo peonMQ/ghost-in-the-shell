25fb0f10d1224f5206f599eedd2f96d91ffcee0f
Aleksander Spro <abspro@gmail.com>
2023-08-12 18:38:33 +0200

    Start PIDS with Hud

--- init.lua
+++ init.lua
@@ -3,10 +3,19 @@ local mq = require 'mq'
 local plugins = require 'utils/plugins'
 local logger = require 'utils/logging'
 local luapaths = require 'utils/lua-paths'
+local broadCastInterfaceFactory = require('broadcast/broadcastinterface')
 
 ---@type RunningDir
 local runningDir = luapaths.RunningDir:new()
 runningDir:AppendToPackagePath()
+
+local bci = broadCastInterfaceFactory()
+if not bci then
+  logger.Fatal("No networking interface found, please start eqbc or dannet")
+  return
+end
+
+bci.ExecuteAllCommand('/lua run hud/pids')
 
 mq.cmd("/lua run hud")
 
