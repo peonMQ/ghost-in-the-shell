765151b76333ee345ccf2803467ab98a92e6b902
Aleksander Spro <abspro@gmail.com>
2023-10-29 18:33:20 +0100

    Clean up dead code

--- lib/moveutils.lua
+++ lib/moveutils.lua
@@ -1,9 +1,6 @@
---- @type Mq
 local mq = require 'mq'
 local logger = require 'utils/logging'
 local plugins = require 'utils/plugins'
-
---- @type Timer
 local timer = require 'lib/timer'
 
 plugins.EnsureIsLoaded("mq2moveutils")
@@ -25,37 +22,6 @@ local function moveToMe()
   end
 
   mq.cmdf("/bca //nav id %d", me.ID())
-end
-
----@deprecated
-local function moveToLoc_dep(xLoc, yLoc, maxTime, arrivalDist)
-  if(not xLoc or not yLoc) then
-    logger.Debug("Cannot move to location <x:%d> <y:%d>", xLoc, yLoc)
-    return false
-  end
-
-  local distanceDelta = arrivalDist or 10
-  local maxTryTime = maxTime or 3
-
-  logger.Debug("Distance to loc <x:%d> <y:%d> => <%d>", xLoc, yLoc, mq.TLO.Math.Distance(string.format('%d,%d', (yLoc), (xLoc)))())
-  if arrivedAtDestination(xLoc, yLoc, distanceDelta) then
-    return true
-  end
-
-  if mq.TLO.Me.Casting.ID() and mq.TLO.Me.Class.ShortName() ~= "BRD" then
-    mq.cmd("/stopcast")
-  end
-
-  local timeOut = timer:new(maxTryTime)
-
-  local moveCmd = string.format("/moveto loc %d %d dist %d", yLoc, xLoc, distanceDelta)
-  while not arrivedAtDestination(xLoc, yLoc, distanceDelta) and timeOut:IsRunning() do
-    mq.cmd(moveCmd)
-    mq.delay(maxTryTime * 1000 / 5, function() return arrivedAtDestination(xLoc, yLoc, distanceDelta) end)
-  end
-
-  mq.cmd("/moveto off")
-  return arrivedAtDestination(xLoc, yLoc, distanceDelta)
 end
 
 local function moveToLoc(xLoc, yLoc, zLoc, maxTime, arrivalDist)
@@ -106,7 +72,6 @@ local function moveToLoc(xLoc, yLoc, zLoc, maxTime, arrivalDist)
 end
 
 local moveUtils = {
-  MoveToLocDep = moveToLoc_dep,
   MoveToLoc = moveToLoc,
   MoveTome = moveToMe
 }
