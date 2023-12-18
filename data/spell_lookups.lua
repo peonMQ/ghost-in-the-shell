a627b5d78b440dbcde48a0c91a8b7b52e3fa1e2e
Aleksander Spro <abspro@gmail.com>
2023-12-18 21:03:56 +0100

    Add type def

--- data/spell_lookups.lua
+++ data/spell_lookups.lua
@@ -1,5 +1,7 @@
 local spellGroups = require 'data/spell_groups'
 
+---@param spellGroups table<ClassShortNames, string[]>
+---@return table<string, ClassShortNames>
 local function extractLookUps(spellGroups)
   local lookups = {}
   for class, groups in pairs(spellGroups) do
