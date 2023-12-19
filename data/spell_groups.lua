b81830c78a77658db213ed0d17831fba54e8ff16
Aleksander Spro <abspro@gmail.com>
2023-12-19 12:50:23 +0100

    Fix definition

--- data/spell_groups.lua
+++ data/spell_groups.lua
@@ -24,7 +24,7 @@ local function extractGroups(classGroups)
   return groups
 end
 
----@type table<ClassShortNames, string[]>
+---@type table<ClassShortNames, table<string, string[]>>
 local SpellGroups = {
   BRD = bardSpells,
   BST = beastlordSpells,
