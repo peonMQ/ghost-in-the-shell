5f8c3b23853533970b75b6a9091b9b074a2353b9
Aleksander Spro <abspro@gmail.com>
2024-08-12 22:23:16 +0200

    Plugin defs

---
+++ plugins/ActorFollow/_TLO.lua
@@ -0,0 +1,10 @@
+---@meta
+
+--- ActorFollow Lua Bindings
+---@class TLO.ActorFollow
+--- @field public IsActive MQBoolean
+--- @field public Status MQString
+--- @field public WaypointsCount MQInt
+--- @field public IsFollowing MQBoolean
+--- @field public IsPaused MQBoolean
+TLO.ActorFollow = {}
