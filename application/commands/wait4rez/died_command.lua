98814c29b4c003477c51abdb9d1c99435b3fac8a
Aleksander Spro <abspro@gmail.com>
2024-12-03 14:51:36 +0100

    Check for zoned before trying to mem spells

--- application/commands/wait4rez/died_command.lua
+++ application/commands/wait4rez/died_command.lua
@@ -6,7 +6,19 @@ local memorize_command  = require('application/commands/memorize_command')
 local loot_corpse_command  = require('application/commands/wait4rez/loot_corpse_command')
 local binder = require('application/binder')
 
+
 local function waitToZone()
+	logger.Debug("Waiting to zone.")
+  local me = mq.TLO.Me.Name()
+  repeat
+    mq.delay(100)
+  until not mq.TLO.Spawn(me.."'s").ID()
+
+  mq.delay(500)
+  logger.Debug("Completed zoneing to corpse.")
+end
+
+local function waitToZoneToCorpse()
 	logger.Debug("Waiting to zone.")
   local me = mq.TLO.Me.Name()
   repeat
@@ -20,17 +32,16 @@ end
 local function execute()
   broadcast.FailAll("%s died, awaiting rez.", mq.TLO.Me.Name())
   mq.cmd("/beep")
-
-  broadcast.WarnAll("Ready for rezz.")
+  waitToZone()
   mq.cmd("/consent guild")
-
   memorize_command()
+  broadcast.WarnAll("Ready for rezz.")
   repeat
     mq.delay(10)
   until mq.TLO.Window("ConfirmationDialogBox").Open() and mq.TLO.Window("ConfirmationDialogBox").Child("cd_textoutput").Text():find("percent)")
 
   mq.cmd("/nomodkey /notify ConfirmationDialogBox Yes_Button leftmouseup")
-  waitToZone()
+  waitToZoneToCorpse()
   broadcast.SuccessAll("Ressurected, ready to loot corpse.")
   loot_corpse_command()
 end
