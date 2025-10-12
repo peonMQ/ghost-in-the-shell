e1d19f4de88f821eb346121823dd4cc4236de1ca
Aleksander Spro <abspro@gmail.com>
2025-10-12 11:11:10 +0200

    Add lockpick ui action

---
+++ application/commands/movement/lockpick_nearest_door.lua
@@ -0,0 +1,162 @@
+local mq = require('mq')
+local logger = require('knightlinc/Write')
+local broadcast = require('broadcast/broadcast')
+local broadCastInterfaceFactory = require("broadcast/broadcastinterface")
+local plugins = require('utils/plugins')
+local commandQueue  = require('application/command_queue')
+local binder = require('application/binder')
+local assist = require('core/assist')
+local movement = require('core/movement')
+local settings = require('settings/settings')
+
+local bci = broadCastInterfaceFactory('ACTOR')
+
+local lockpicks = "Lockpicks" -- 13010
+local mechanizedLockpicks= "Mechanized Lockpicks" --16865
+
+local function execute()
+    logger.Info('Start [DoLockPick]')
+
+    local lockItem = nil
+    if mq.TLO.FindItemCount("="..mechanizedLockpicks)() > 0 then
+        lockItem = mechanizedLockpicks
+    elseif mq.TLO.FindItemCount("="..lockpicks)() > 0 then
+        lockItem = lockpicks
+    end
+
+    if not lockItem then
+        broadcast.WarnAll('|- No Lockpicks set found.')
+        return
+    end
+
+    if mq.TLO.Me.Skill('Pick Lock')() == nil then
+        logger.WarnAll('|- You do not have the skill: Pick Lock')
+        return
+    end
+
+    local maxMoveTime = 5
+    -- Target the nearest door
+    mq.cmd('/doortarget')
+    if mq.TLO.DoorTarget.Distance() > 20 and not movement.MoveToLoc(mq.TLO.DoorTarget.Y(), mq.TLO.DoorTarget.X(), maxMoveTime, 15) then
+        logger.Error("|- I could not moveto < 20 units of %s within %ss", mq.TLO.DoorTarget.Name(), maxMoveTime)
+        mq.cmd("/beep")
+        return
+    end
+
+    if (mq.TLO.DoorTarget.Distance() > 35) then
+        logger.Error("|- I could not moveto < 20 units of %s within %ss", mq.TLO.DoorTarget.Name(), maxMoveTime)
+        mq.cmd("/beep")
+        return
+    end
+
+    -- Lockpicks - 13010
+    local invSlot = mq.TLO.FindItem("="..lockpicks).InvSlot()
+    mq.cmd(string.format("/nomodkey /itemnotify %s leftmouseup", invSlot))
+    mq.delay(2000, function() return mq.TLO.Cursor() == lockpicks end)
+    if mq.TLO.Cursor() ~= lockpicks then
+        logger.Error('|- Could not pick up <Lockpicks> on cursor.')
+        mq.cmd("/beep")
+    end
+
+    mq.cmd("/click left door")
+    mq.cmd(string.format("/nomodkey /itemnotify %s leftmouseup", invSlot))
+    mq.delay(2000, function() return not mq.TLO.Cursor() end)
+    logger.Info('End [DoLockPick]')
+end
+
+local function createCommand()
+    if settings.isLockpicker then
+      commandQueue.Enqueue(function() execute() end)
+    end
+end
+
+binder.Bind("/lockpick", createCommand, 'Tells the bot to lockpick the nearest door if its within range of it and is able to.')
+
+local mq = require('mq')
+local logger = require('knightlinc/Write')
+local broadcast = require('broadcast/broadcast')
+local broadCastInterfaceFactory = require("broadcast/broadcastinterface")
+local plugins = require('utils/plugins')
+local commandQueue  = require('application/command_queue')
+local binder = require('application/binder')
+local assist = require('core/assist')
+local movement = require('core/movement')
+local settings = require('settings/settings')
+
+local bci = broadCastInterfaceFactory('ACTOR')
+
+local lockpicks = "Lockpicks" -- 13010
+local mechanizedLockpicks= "Mechanized Lockpicks" --16865
+
+local function execute()
+    logger.Info('Start [DoLockPick]')
+
+    local lockpickItem = mq.TLO.FindItem("="..mechanizedLockpicks)
+    if not lockpickItem() then
+        lockpickItem = mq.TLO.FindItem("="..lockpicks)
+    end
+
+    if not lockpickItem() then
+        broadcast.WarnAll('|- No Lockpicks set found.')
+        logger.Info('End [DoLockPick]')
+        return
+    end
+
+    if mq.TLO.Me.Skill('Pick Lock')() == nil then
+        logger.WarnAll('|- You do not have the skill: Pick Lock')
+        logger.Info('End [DoLockPick]')
+        return
+    end
+
+    local maxMoveTime = 5
+    -- Target the nearest door
+    mq.cmd('/doortarget')
+    if mq.TLO.DoorTarget.Distance() > 20 and not movement.MoveToLoc(mq.TLO.DoorTarget.Y(), mq.TLO.DoorTarget.X(), maxMoveTime, 15) then
+        logger.Error("|- I could not moveto < 20 units of %s within %ss", mq.TLO.DoorTarget.Name(), maxMoveTime)
+        mq.cmd("/beep")
+        logger.Info('End [DoLockPick]')
+        return
+    end
+
+    if (mq.TLO.DoorTarget.Distance() > 35) then
+        logger.Error("|- I could not moveto < 20 units of %s within %ss", mq.TLO.DoorTarget.Name(), maxMoveTime)
+        mq.cmd("/beep")
+        logger.Info('End [DoLockPick]')
+        return
+    end
+
+    local packslot = lockpickItem.ItemSlot() - 22
+    local inpackslot = lockpickItem.ItemSlot2();
+    if(inpackslot >= 0) then
+      mq.cmdf("/nomodkey /itemnotify in pack%d %d leftmouseup", packslot, inpackslot + 1)
+    else
+      mq.cmdf("/nomodkey /itemnotify pack%d leftmouseup", packslot)
+    end
+
+    mq.delay(2000, function() return mq.TLO.Cursor() == lockpickItem.Name() end)
+    if mq.TLO.Cursor() ~= lockpickItem.Name() then
+        logger.Error('|- Could not pick up <%s> on cursor.', lockpickItem.Name())
+        mq.cmd("/beep")
+    end
+
+    mq.cmd("/click left door")
+    if(inpackslot >= 0) then
+      mq.cmdf("/nomodkey /itemnotify in pack%d %d leftmouseup", packslot, inpackslot + 1)
+    else
+      mq.cmdf("/nomodkey /itemnotify pack%d leftmouseup", packslot)
+    end
+
+    mq.delay(2000, function() return not mq.TLO.Cursor() end)
+    if mq.TLO.Cursor() then
+        logger.Fatal('|- Unable to clear cursor for <%s>.', lockpickItem.Name())
+    end
+    logger.Info('End [DoLockPick]')
+end
+
+local function createCommand()
+    if settings.isLockpicker then
+      commandQueue.Enqueue(function() execute() end)
+    end
+end
+
+binder.Bind("/lockpick", createCommand, 'Tells the bot to lockpick the nearest door if its within range of it and is able to.')
---
+++ ui/actions/11_lockpick.lua
@@ -0,0 +1,22 @@
+local icons = require('mq/Icons')
+local bci = require('broadcast/broadcastinterface')('ACTOR')
+local app_state = require('app_state')
+local buttons = require('ui/buttons')
+
+---@type ActionButton
+local clickdoor = {
+  active = false,
+  icon = icons.MD_LOCK_OPEN,
+  tooltip = "Lockpick Nearest Door",
+  isDisabled = function () return  not app_state.IsActive() end,
+  activate = function()
+    bci.ExecuteZoneCommand('/lockpick')
+  end,
+}
+
+return {
+  ---@param buttonSize ImVec2
+  Render = function (buttonSize)
+    buttons.CreateButton(clickdoor, buttons.BlueButton, buttonSize)
+  end,
+}
