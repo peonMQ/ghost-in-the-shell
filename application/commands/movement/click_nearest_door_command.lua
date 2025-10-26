87aad941c0ca3a44d8ad36cb9dc751f9e7f1c917
Aleksander Spro <abspro@gmail.com>
2025-10-26 13:02:27 +0100

    Handle click nearest door for HS

--- application/commands/movement/click_nearest_door_command.lua
+++ application/commands/movement/click_nearest_door_command.lua
@@ -3,15 +3,30 @@ local logger = require('knightlinc/Write')
 local plugins = require('utils/plugins')
 local commandQueue  = require('application/command_queue')
 local binder = require('application/binder')
+local worldZones = require('data/zones')
 
-local function execute()
-  mq.cmdf("/doortarget")
+local function tryFindDoorName()
+  if mq.TLO.Zone.ShortName() == "overthere" then
+    return "HOWLER"
+  end
+
+  return nil
+end
+
+---@param doorName string|nil
+local function execute(doorName)
+  if not doorName then
+    mq.cmd("/doortarget")
+  else
+    mq.cmdf("/doortarget %s", doorName)
+  end
+
   mq.delay(50)
   mq.cmdf("/click left door")
 end
 
 local function createCommand()
-    commandQueue.Enqueue(function() execute() end)
+    commandQueue.Enqueue(function() execute(tryFindDoorName()) end)
 end
 
 binder.Bind("/clickdoor", createCommand, 'Tells the bot to click the nearest door if its within range of it.')
