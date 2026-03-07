00de47eff7a4fedf4f195f911d62dc312a85a5d4
Aleksander Spro <abspro@gmail.com>
2026-03-07 09:06:14 +0100

    Better handle follow mode (and stick from cbt)

--- core/movement.lua
+++ core/movement.lua
@@ -2,7 +2,18 @@ local mq = require('mq')
 local logger = require('knightlinc/Write')
 local plugins = require('utils/plugins')
 local timer = require('core/timer')
+local follow_state = require('application/follow_state')
+local assist_state = require('application/assist_state')
 local bci = require('broadcast/broadcastinterface')('REMOTE')
+
+--- Calculates the squared distance between two points (x1, y1) and (x2, y2).
+--- This is useful for distance comparisons without the computational cost of a square root.
+--- @param loc1 ImVec4 The first location
+--- @param loc ImVec4 The second location.
+--- @return number The squared distance between the two points.
+local function getDistanceSquared(loc1, loc)
+  return ((loc.x or 0) - (loc1.x or 0)) ^ 2 + ((loc.y or 0) - (loc1.y or 0)) ^ 2
+end
 
 local function arrivedAtDestination(xLoc, yLoc, distanceDelta)
   local distance = mq.TLO.Math.Distance(string.format('%d,%d', (yLoc), (xLoc)))()
@@ -69,6 +80,9 @@ local function moveToLoc(xLoc, yLoc, zLoc, maxTime, arrivalDist)
   return arrivedAtDestination(xLoc, yLoc, distanceDelta)
 end
 
+local lastPosition = ImVec4(-1, -1, -1, -1)
+local followTimer = timer:new(2);
+
 local function isFollowing()
   if plugins.IsLoaded("mq2nav") and mq.TLO.Navigation.Active() then
     return true
@@ -82,13 +96,28 @@ local function isFollowing()
     return true
   end
 
-  if plugins.IsLoaded("mq2moveutils") and mq.TLO.Stick.Active() then
+  if plugins.IsLoaded("mq2moveutils") and mq.TLO.Stick.Active() and follow_state:IsActive() then
     local stickSpawn = mq.getFilteredSpawns(function(spawn) return spawn.ID() == mq.TLO.Stick.StickTarget() and  spawn.Type() =="PC" end)
     if next(stickSpawn) then
       return true
     end
   end
 
+  local currentPosition = ImVec4(mq.TLO.Me.X(), mq.TLO.Me.Y(), mq.TLO.Me.Z(), 0)
+  if follow_state:IsActive() and assist_state.current_target_id <= 0 then
+    if lastPosition ~= currentPosition then
+      logger.Trace("Following, moved from <%d, %d, %d> to <%d, %d, %d>", lastPosition.x, lastPosition.y, lastPosition.z, currentPosition.x, currentPosition.y, currentPosition.z)
+      lastPosition = currentPosition
+      followTimer:Reset()
+      return true
+    elseif lastPosition == currentPosition and followTimer:IsRunning() then
+      logger.Trace("Following, waiting at <%d, %d, %d> with timer %d", lastPosition.x, lastPosition.y, lastPosition.z, followTimer:TimeRemaining())
+      lastPosition = currentPosition
+      return true
+    end
+  end
+
+  lastPosition = currentPosition
   return false
 end
 
